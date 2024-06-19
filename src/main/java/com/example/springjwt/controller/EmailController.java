2d6bf46a5c311ca48ac2283118f9961b31e1beb4
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-06-20 01:05:45 +0900

    study

---
+++ src/main/java/com/example/springjwt/controller/EmailController.java
@@ -0,0 +1,21 @@
+package com.example.springjwt.controller;
+
+import data.service.EmailService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/emailConfirm")
+    public String mailConfirm(@RequestBody JoinRequest joinRequest) {
+        int num = emailService.sendEmail(joinRequest.getID());
+
+        return "코드 발급" + num;
+    }
+}
